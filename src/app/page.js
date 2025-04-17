f96905c241d940e010f454265055a90bdd879672
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-17 19:14:11 -0300

    feat(components): Testar função map para componentes Next

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,29 @@ import styles from "./page.module.css";
 // }
 
 export default function Main(){
-    return <ComponentWithVar data="Teste"/>;
+    // return <ComponentWithVar data="Teste"/>;
+
+	const virtues = ['Sabedoria', 'Justiça', 'Inteligência'];
+    return <List data={virtues}/>;
 }
 
-function ComponentWithVar({data}){
-	return <>
-		I'm a ComponentWithVar: {data}
-	</>
+// function ComponentWithVar({data}){
+// 	return <>
+// 		I'm a ComponentWithVar: {data}
+// 	</>
+// }
+
+function List(virtues){
+	console.log(virtues);
+	
+	const listItems = virtues.data.map((value) => {
+		console.log(value);
+		return <li>
+			{value}
+		</li>
+	})
+
+	return <ul>
+		{listItems}
+	</ul>
 }
