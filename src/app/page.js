cdcd44cad1db272eeff413e7915a83f086e7161b
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-18 19:06:31 -0300

    feat(logo): Colocar imagem da logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-// export default function HelloWorld() {
-//   return (
-//     <div>
-//       Hello, World
-//     </div>
-//   )
-// }
+export default function LoginPage(){
+    return <main className="login-container">
+		<form className="login-container__logo-box" style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			minHeight: '100vh',
+			flexDirection: 'column',
+			gap: '1rem',
+		}}>
+			<Image 
+			src="./employ.svg"
+			alt="employ logo com um ícone de conta envolvido"
+			width={200}
+			height={200}
+			/>
 
-export default function Main(){
-    // return <ComponentWithVar data="Teste"/>;
 
-	const virtues = ['Sabedoria', 'Justiça', 'Inteligência'];
-    return <List data={virtues} style={{
-			color: 'red',
-			backgroundColor: '#000'}}/>;
-}
-
-// function ComponentWithVar({data}){
-// 	return <>
-// 		I'm a ComponentWithVar: {data}
-// 	</>
-// }
-
-function List(virtues){
-	console.log(virtues);
-	
-	const listItems = virtues.data.map((value) => {
-		console.log(value);
-		return <li>
-			{value}
-		</li>
-	})
-
-	return <ul>
-		{listItems}
-	</ul>
+		</form>
+	</main>
 }
