0ec346283d4736d1cecb792a004878c10ab13147
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-19 16:07:40 -0300

    refactor(class): Adicionar classes CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./general.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 function Form() {
 	return (
-		<form className="login-container__logo-box container" style={{
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			minHeight: '100vh',
-			flexDirection: 'column',
-			gap: '1rem',
-		}}>
+		<form className="login-container__logo-box container g-1rem">
 			<Image
 				src="./employ.svg"
 				alt="employ logo com um ícone de conta envolvido"
@@ -18,21 +10,27 @@ function Form() {
 				height={200}
 			/>
 
-		<Inputs></Inputs>
+			<Inputs></Inputs>
+
+			<Button className='login-container__login-btn' type='submit'>
+				Acessar
+			</Button>
 		</form>)
 }
 
-function Input({name, type}){
+function Input({ name, type }) {
 	const labelName = name;
-	
-	return (<label>
-			{labelName}
-			<input name={name} type={type}/>
-		</label>)
+
+	return (<label className="input-box login-container__label">
+		{labelName}
+		<input name={name} type={type} className="input login-container__input" />
+	</label>)
 }
 
 function Inputs() {
-	return (<section className="login-container__inputs container">
+	return (<section className={
+		`login-container__inputs container`
+	}>
 		<Input name='email' type='email' />
 		<Input name='password' type='password' />
 	</section>)
@@ -46,6 +44,7 @@ function Button({ children, className, type }) {
 		</button>
 	)
 }
+
 export default function LoginPage() {
 	return <main className="login-container container">
 		<Form></Form>
