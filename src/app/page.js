c7609a02afc5c5906576e9c02de00a8e4cd2e13e
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-19 16:51:28 -0300

    feat(label): Melhorar exibição dos labels

--- src/app/page.js
+++ src/app/page.js
@@ -22,12 +22,21 @@ function Form() {
 }
 
 function Input({ name, type }) {
-	const labelName = name;
-
+	const labelName = setLabelName(name);
 	return (<label className="input-box login-container__label">
 		{labelName}
 		<input name={name} type={type} className="input login-container__input" />
 	</label>)
+}
+
+function setLabelName(name){
+	switch(name){
+		case 'email':
+			return 'Email';
+		case 'password': 
+			return 'Senha';
+		default: return 'Label'
+	};
 }
 
 function Inputs() {
