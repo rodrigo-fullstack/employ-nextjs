5ed0713f9d0cfac61d86de5f480b86dd2e0759d8
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-21 18:03:06 -0300

    fix: Corrigir formData passando parâmetros corretos

--- src/app/_services/actions.js
+++ src/app/_services/actions.js
@@ -3,15 +3,14 @@
 import { redirect } from "next/dist/server/api-utils/index.js";
 import AuthService from "./AuthService.js";
 
-export async function login(formData) {
+export async function login(previousState, formData) {
     const response = await AuthService.login(formData);
-
     // if(response.error){
     //     return response;
     // }
 
     return response;
-    redirect('/dashboard');
+    // redirect('/dashboard');
     
 
 }
