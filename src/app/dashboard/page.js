797e517aa402ddb95d1cc99013b74dac8aeea682
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-21 18:54:53 -0300

    feat(dashboard): Iniciar construção do dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Container from "../_components/Container";
+
+function Dashboard(){
+    return (<Container>
+        <div className="container">Teste</div>
+    </Container>);
+}
+
+export default function DashboardPage(){
+    return (
+        <Dashboard />
+    )
+}
