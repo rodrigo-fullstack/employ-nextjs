5fb3a6921fa40d2728d29c9029532aef8fb784ea
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-21 15:35:04 -0300

    refactor: Refatorar estrutura das requisições para melhorar resposta nos componentes

--- src/app/_services/ApiService.js
+++ src/app/_services/ApiService.js
@@ -1,44 +1,21 @@
 
 
-export default class ApiService{
+export default class ApiService {
     static baseUrl = 'http://localhost:8000/api';
-    
-    static async makeRequest(url, method, body = null){
+
+    static async makeRequest(url, method, body = null) {
         const options = {
             method: method,
-            headers: {
-                Accept: 'application/json'
-            }
+            headers: { Accept: 'application/json' }
         }
-        
-        if(body){
-            options.body = body;
+        if (body) options.body = body;
+        const response = await fetch(url, options);
+        const jsonText = await response.text();
+        const json = JSON.parse(jsonText);
+        if (!json.error && !response.ok) {
+            json.error = true;
+            json.success = false;
         }
-
-        try{
-            const response = await fetch(url, options);
-
-            if(!response.ok){
-                try{
-                    let jsonError = await response.json();
-                    throw new Error(jsonError.message);
-                } catch(e){
-                    console.error('Json Inválido');
-                    throw e;
-                }
-            }
-
-            const jsonText = await response.text();
-            console.log(jsonText);
-            return;
-            const json = JSON.parse(jsonText);
-            if(json.error){
-                throw new Error(json.message);
-            }
-
-            return json;
-        } catch(e){
-            console.error(e);
-        }
+        return json;
     }
 }
