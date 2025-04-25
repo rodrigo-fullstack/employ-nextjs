67db032b20c9a4d95a68b6f5f0e5bfadeea51b12
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-25 15:42:41 -0300

    feat(layout): Criar layout base para todas páginas

---
+++ src/app/layout.js
@@ -0,0 +1,16 @@
+import '@/app/general.css';
+import '@/app/globals.css';
+
+export default function RootLayout({children}){
+    return(
+        <html lang="pt-br">
+            <head>
+                <link rel="icon" href="./favicon.ico" />
+            </head>
+            
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
