a23e6677a358d385a00e18e632131c058769cd41
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-26 19:20:57 -0300

    feat(header): Criar cabeçalho e estilizar

--- src/app/_components/dashboard/Header.js
+++ src/app/_components/dashboard/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
         <header className="header dashboard-header">
             <Container className="header__title-container dashboard-header__title-container">
                 <h1 className="title header__title dashboard-header__title">
-                    [Título]
+                    Vagas
                 </h1>
             </Container>
 
@@ -25,11 +25,12 @@ function FormSearch() {
     return (
         <form className="form form--search 
     header__search-form dashboard-header__search-form">
-            <Input className="input--search header__search dashboard-header__search" name="search" hasLabel={false} />
+            <Input className="input--search header__search dashboard-header__search" name="search" hasLabel={false} 
+            placeholder="Pesquisar vagas"/>
             <Button type="submit" className="btn--search header__search-btn dashboard-header__search-btn">
                 <Image
-                    width={32}
-                    height={32}
+                    width={28}
+                    height={28}
                     className="icon icon--search header__search-icon
             dashboard-header__search-icon"
                     src="./search.svg"
@@ -60,10 +61,10 @@ function Profile() {
 function ProfileItem({ children, src = "", alt = "" }) {
     return <div className={children + " header__profile-item dashboard-header__profile-item"}>
         <Image
-            width={32}
-            height={32}
+            width={50}
+            height={50}
             alt={alt}
-            className={"icon icon--" + children + "header__profile-icon dashboard-header__profile-icon"}
+            className={"icon icon--" + children + " header__profile-icon dashboard-header__profile-icon"}
             src={src}
         />
     </div>
