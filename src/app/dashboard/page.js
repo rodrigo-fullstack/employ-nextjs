9feeb9e8f094dfb5c401820b459cacf9cafa8c19
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-28 11:56:26 -0300

    refactor: Refatorar removendo componente de vaga selecionada e imports desnecessários

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,13 +1,14 @@
-import Image from "next/image";
 import Article from "../_components/Article";
 import Container from "../_components/Container";
 import Row from "../_components/Row";
-import Title from "../_components/Title";
-import Button from "../_components/Button";
+import Vacancy from "../_components/dashboard/Vacancy";
+import VacancySelected from "../_components/dashboard/VacancySelected";
 
 function Dashboard() {
     return (<Row className={"main__content"}>
         <Container className="vacancies-container">
+            <Vacancy />
+            <Vacancy />
             <Vacancy />
         </Container>
 
@@ -21,122 +22,3 @@ export default function DashboardPage() {
     )
 }
 
-function Vacancy() {
-    return (
-        <Article className="vacancy vacancies-container__element">
-            <Container className="vacancy__info-container">
-                <VacancyHeader />
-                <VacancyInfoContent />
-
-                
-            </Container>
-
-            <VacancyActions />
-
-            <span className="text vacancy__skills">
-                Competências: PHP, Laravel, React, NextJS, MySQL, SPA, Arquitetura MVC, APIs Rest, Testes com PestPHP, Docker
-            </span>
-
-            <VacancyDescription />
-        </Article>
-    )
-}
-
-function VacancyHeader() {
-    return (
-        <Row className="vacancy__info-header">
-            <Container className="vacancy__info-logo-container">
-                <Image
-                    className="vacancy__info-logo"
-                    src="./vacancy-logo.svg"
-                    alt="Logo da empresa da vaga"
-                    width={50}
-                    height={50}
-                />
-            </Container>
-            <Container className="vacancy__info-title-container">
-                <Title className="vacancy__info-title">
-                    Desenvolvedor Fullstack Junior PHP
-                </Title>
-            </Container>
-        </Row>)
-}
-
-function VacancyInfoContent() {
-    return (
-
-        <Container className="vacancy__info-content">
-            <a className="text vacancy__info-company" href="#">Employ - Encontre a vaga e o candidato perfeito.</a>
-            <Row className="text vacancy__info-general">
-                <span className="text vacancy__info-text vacancy__info-local">Salvador/Bahia</span>
-
-                <span className="text vacancy__info-text vacancy__info-local">R$4000,00</span>
-
-                <span className="text vacancy__info-text vacancy__info-applications">Mais de 25 candidatos</span>
-
-                <span className="text vacancy__info-text vacancy__info-presence">Presencial</span>
-
-                <span className="text vacancy__info-text vacancy__info-contract">CLT</span>
-
-                <span className="text vacancy__info-text vacancy__info-time">Data de Publicação: 28/04/2025</span>
-
-                <span className="text vacancy__info-text vacancy__info-time">Expira em: 28/05/2025</span>
-            </Row>
-        </Container>
-    )
-}
-
-function VacancyActions() {
-    return (
-        <Row className="vacancy__actions">
-            <Button className="btn--vacancy vacancy__btn">
-                Candidatar-se
-            </Button>
-
-            <Row className="vacancy__btn-actions">
-                <span className="vacancy__btn-action btn--save">
-                    <Image 
-                    width={32}
-                    height={32}
-                    src="./bookmark.svg"
-                    alt="Botão de salvar: Marcador de livro"
-                    />
-                </span>
-                
-                <span className="vacancy__btn-action btn--share">
-                    <Image 
-                    width={32}
-                    height={32}
-                    src="./share.svg"
-                    alt="Botão de salvar: Marcador de livro"
-                    />
-                </span>
-                
-                <span className="vacancy__btn-action btn--more">
-                    <Image 
-                    width={32}
-                    height={32}
-                    src="./more.svg"
-                    alt="Botão de salvar: Marcador de livro"
-                    />
-                </span>
-            </Row>
-        </Row>
-    )
-}
-
-function VacancyDescription() {
-    return (
-        <Container className="vacancy__description">
-            Busca-se um desenvolvedor fullstack de PHP que esteja disposto a colaborar em um ambiente ágil de desenvolvimento de software para um website de gerenciamento de vagas e candidaturas.
-        </Container>
-    )
-}
-
-function VacancySelected(){
-    return (
-        <Article className="vacancy-selected-container">
-            abc
-        </Article>
-    )
-}
