d805e752d3a0489126b9a6fb0475b8109a5bfec7
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 09:47:23 -0300

    refactor: Refatorar article

--- src/app/_components/Article.js
+++ src/app/_components/Article.js
@@ -1,5 +1,7 @@
+import styles from "./Container.module.css";
+
 export default function Article({children, className}){
-	return (<article className={"container " + className}>
+	return (<article className={`${styles.container} ${className}`}>
 		{children}
 	</article>);
 }
