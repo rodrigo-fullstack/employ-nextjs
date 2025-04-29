7bb43beee9716d20882e33fdc629bee72eed5b5e
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 10:32:23 -0300

    refactor: Refatorar Input

--- src/app/_components/Input.js
+++ src/app/_components/Input.js
@@ -1,6 +1,6 @@
 import List from "./List.js";
 import ErrorItem from "./ErrorItem.js";
-
+import styles from "./Form.module.css";
 
 export default function Input({
 	name = "",
@@ -14,63 +14,9 @@ export default function Input({
 		<input 
 		type={type}
 		name={name}
-		className={"input " + className}
+		className={`${styles.input} ${className}`}
 		placeholder={placeholder}
 		{...props}
 		/>
 	)
-}
-
-function InputOld(
-	{
-		className = '',
-		name,
-		type = 'text',
-		forgotPassword = false,
-		errors = null,
-		hasLabel = true,
-		placeholder = ""
-	}
-) {
-	let labelName = null;
-	if (hasLabel) {
-		labelName = setLabelName(name);
-	}
-	let errorList = null;
-	if (errors) {
-		let errorsItems = errors.map(element =>
-			(<ErrorItem>{element}</ErrorItem>)
-		);
-		errorList = <List className="list--error">
-			{errorsItems}
-		</List>
-	}
-
-
-	return (<label className="input-box">
-		{forgotPassword ?
-			(<div className="row">
-				{labelName && <span>{labelName}</span>}
-				<span className="bold login__forgot-password">Esqueceu a senha?</span>
-			</div>)
-			: labelName && <span>{labelName}</span>
-		}
-		<input
-			name={name}
-			type={type}
-			className={"input " + className} 
-			placeholder={placeholder}
-			/>
-		{errorList ?? ''}
-	</label>)
-}
-
-function setLabelName(name) {
-	switch (name) {
-		case 'email':
-			return 'Email';
-		case 'password':
-			return 'Senha';
-		default: return name
-	};
 }
