919f2655e6ce6d2241e25d0f400273e376f8bfe2
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 10:40:56 -0300

    refactor: Refatorar login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,7 +6,8 @@ import Container from "../_components/Container.js";
 import Input from "../_components/Input.js";
 import FormField from "../_components/FormField.js";
 import Title from "../_components/Title.js";
-import '../form.css';
+import styles from '@/app/_components/Form.module.css'
+import containerStyles from '@/app/_components/Container.module.css'
 import '../auth.css';
 import { login } from "../_services/actions.js";
 import { useActionState } from "react";
@@ -14,9 +15,9 @@ import { useActionState } from "react";
 function Form() {
 	const [data, formAction] = useActionState(login, null);
 
-	if(data) console.log(data);
+	if (data) console.log(data);
 	return (
-		<form className="login__form form container" action={formAction}>
+		<form className={`${styles['form']} ${styles['form--auth']} login__form form ${containerStyles.container}`} action={formAction}>
 			<Container className="login__logo-box">
 				<Image
 					src="./employ.svg"
@@ -31,10 +32,10 @@ function Form() {
 				<Title className="login__title">
 					Login
 				</Title>
-				<hr className="login__hr"/>
+				<hr className="login__hr" />
 			</Container>
 
-			<Inputs errors={data?.errors}/>
+			<Inputs errors={data?.errors} />
 
 			<Button className='login__login-btn' type='submit'>
 				Acessar
@@ -42,22 +43,22 @@ function Form() {
 		</form>)
 }
 
-function Inputs({errors = null}) {
+function Inputs({ errors = null }) {
 	return (<Container className={
 		`login__inputs g-1rem`
 	}>
-		<FormField labelClassName="login__field" labelTextClassName="login__label-text" label="Email" name="email" errors={errors?.email}> 
-			<Input name="email" type="email" className="login__input" placeholder="Ex: rodrigo@gmail.com"/>
+		<FormField labelClassName="login__field" labelTextClassName="login__label-text" label="Email" name="email" errors={errors?.email}>
+			<Input name="email" type="email" className="login__input" placeholder="Ex: rodrigo@gmail.com" />
 		</FormField>
-		
-		<FormField labelClassName="login__field login__field--password" labelTextClassName="login__label-text" label="Senha" name="password" errors={errors?.password}> 
-			<Input name="password" type="password" className="login__input"/>
+
+		<FormField labelClassName="login__field login__field--password" labelTextClassName="login__label-text" label="Senha" name="password" errors={errors?.password}>
+			<Input name="password" type="password" className="login__input" />
 		</FormField>
 	</Container>)
 }
 
 export default function LoginPage() {
-	return <main className="login container">
+	return <main className={`login ${containerStyles.container}`}>
 		<Form></Form>
 	</main>
 }
