b03357d30b43cf4714fcb8df7dcaf15875710775
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 09:28:40 -0300

    refactor: Importar fontes pelo next
    - Objetivo: Evitar erro de Layouts

---
+++ src/app/fonts.js
@@ -0,0 +1,6 @@
+import { Open_Sans, Montserrat } from "next/font/google";
+
+export const primaryFont = Open_Sans({ subsets: ['latin']}); 
+
+export const secondaryFont = Montserrat({ subsets: ['latin'] });
+
