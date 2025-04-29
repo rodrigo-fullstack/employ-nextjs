761bbd3200bd2f48f3fcd54fa5083cf4d5c15880
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 09:47:03 -0300

    refactor: Refatorar lista

--- src/app/_components/List.js
+++ src/app/_components/List.js
@@ -1,5 +1,7 @@
+import styles from "./List.module.css";
+
 export default function List({className, children}){
-    return (<ul className={"list " + className}>
+    return (<ul className={`${styles.list} ${className}`}>
         {children}
     </ul>)
 }
