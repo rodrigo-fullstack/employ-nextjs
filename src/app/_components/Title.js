f602186970d012dec7f9161b82535205e0f85ac0
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-29 09:30:05 -0300

    refactor: Reestrutar título com módulo

--- src/app/_components/Title.js
+++ src/app/_components/Title.js
@@ -1,5 +1,9 @@
+import styles from "./title.module.css";
+import { secondaryFont } from "../fonts.js";
+
 export default function Title({children, className}){
-	return (<h1 className={"title " + className}>
+	className = `${styles.title} ${secondaryFont.className} ${className}`;
+	return (<h1 className={className}>
 		{children}
 	</h1>);
 }
