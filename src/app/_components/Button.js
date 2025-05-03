a13750f52e1f779f0528966abf604b2810b36925
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-05-03 12:43:47 -0300

    refactor: Adicionar onClick event

--- src/app/_components/Button.js
+++ src/app/_components/Button.js
@@ -1,9 +1,9 @@
 import styles from "./Button.module.css";
 
-export default function Button({ children, className, type }) {
+export default function Button({ children = '', className = '', type = 'button', action = null}) {
 	const newClassName = `${styles.btn} ${className}`
 	return (
-		<button type={type} className={newClassName}>
+		<button type={type} className={newClassName} onClick={action}>
 			{children}
 		</button>
 	)
